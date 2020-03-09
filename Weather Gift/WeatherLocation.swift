2aa3c95215fcf4961145191ba8bda75f8cea80d3
Benjamin Tsai <benjamintsa@gmail.com>
2020-03-08 23:46:51 -0400

    Setting up app (7.2)

---
+++ Weather Gift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  Weather Gift
+//
+//  Created by Ben Tsai on 3/8/20.
+//  Copyright © 2020 Ben Tsai. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
