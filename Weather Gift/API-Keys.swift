68ff3cc9a012630e0ab2cbb44895042e9f6515cb
Benjamin Tsai <benjamintsa@gmail.com>
2020-03-14 16:03:50 -0400

    with gitignore (7.4)

---
+++ Weather Gift/API-Keys.swift
@@ -0,0 +1,17 @@
+//
+//  API-Keys.swift
+//  Weather Gift
+//
+//  Created by Ben Tsai on 3/14/20.
+//  Copyright © 2020 Ben Tsai. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class API_Keys: UIViewController {
+    struct APIkeys{
+        static let googlePlacesKey = "AIzaSyCmVJP4bm2vlY2osPC45tXZVpHD2enYLgU"
+    }
+    
+}
