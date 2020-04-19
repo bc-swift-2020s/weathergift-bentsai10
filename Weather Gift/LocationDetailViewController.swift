470903b31703b86ae1a497ab6ac83b61f014b8b8
Benjamin Tsai <benjamintsa@gmail.com>
2020-04-18 20:44:33 -0700

    Directing user from app to settings (7.20)

--- Weather Gift/LocationDetailViewController.swift
+++ Weather Gift/LocationDetailViewController.swift
@@ -153,12 +153,27 @@ extension LocationDetailViewController: CLLocationManagerDelegate{
         case .restricted:
             self.oneButtonAlert(title: "Location services denied", message: "It may be that parental controls are restricting location use in this app")
         case .denied:
-            break
+            showAlertToPrivacySettings(title: "User has not authorized location services", message: "Select settings below to authorize location services")
         case .authorizedAlways, .authorizedWhenInUse:
             locationManager.requestLocation()
         @unknown default:
             print("ERROR: DEVELOPER ALERT")
         }
+    }
+    
+    func showAlertToPrivacySettings(title: String, message: String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else{
+            print("Error: Something went wrong opening settings")
+            return
+        }
+        let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_)in
+            UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(settingsAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
